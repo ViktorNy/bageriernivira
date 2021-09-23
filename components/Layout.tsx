ab003d15d693b20d61f7e631d1a36972077d8ca5
Viktor Nyström <95viknys@skola.boras.se>
2021-09-23 15:30:23 +0200

    Basic detail view fixed and navigation to it

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,15 +2,24 @@ import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import HomePage from "../pages/HomePage";
+import DetailPage from "../pages/DetailPage";
+import { Recipe } from "../data";
 
 
 const Layout = () => {
     const [page, setPage] = useState('home');
+    const [selectedRecipe, setRecipe] = useState<Partial<Recipe>>({});
     const goHome = () => setPage('home');
+
+    const setDetailPage = (page: string, recipe: Recipe) => {
+        setPage(page);
+        setRecipe(recipe);
+    }
 
     const selectedPage = () => {
         switch (page) {
-            case 'home': return <HomePage onSetPage={setPage} />;
+            case 'home': return <HomePage onSetPage={setDetailPage} />;
+            case 'details': return <DetailPage onGoBack={goHome} recipe={selectedRecipe as Recipe} />;
         }
     }
 
---
+++ pages/DetailPage.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Text, View, Button, Image, Dimensions, StyleSheet, SafeAreaView, ScrollView } from 'react-native'
+import { Recipe } from '../data';
+
+interface Props {
+    onGoBack: () => void,
+    recipe: Recipe
+}
+
+const DetailPage = ({ onGoBack, recipe }: Props) => {
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <ScrollView contentContainerStyle={styles.scrollViewContainer} style={{ width: '100%' }}>
+                <Text>{recipe.name}</Text>
+                <Image source={{ uri: recipe.imageUrl }} style={{ width: Dimensions.get('screen').width * 0.7, height: 300 }} />
+                <Text style={styles.textStyle}>Beskrivning</Text>
+                <Text>{recipe.description}</Text>
+                <Text style={styles.textStyle}>Ingredienser</Text>
+                <Text>{recipe.ingredients}</Text>
+                <Text style={styles.textStyle}>Gör så här</Text>
+                <Text>{recipe.instructions}</Text>
+                <Button title='Tillbaka' onPress={onGoBack} />
+            </ScrollView>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        padding: 20,
+        flexDirection: 'column'
+    },
+    scrollView: {
+        backgroundColor: 'pink',
+        marginHorizontal: 20,
+    },
+    scrollViewContainer: {
+        display: 'flex',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    opacity: {
+        backgroundColor: '#DDDDDD'
+    }, 
+    textStyle: {
+        fontWeight: 'bold'
+    }
+});
+
+export default DetailPage;
