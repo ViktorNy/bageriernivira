583458eaf738bd8b738a0db468bfe81ec179d3be
Grodan Bool <nils.franzen99@gmail.com>
2021-09-29 11:49:57 +0200

    statusbar hidden and keepawake

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Layout from './components/Layout';
+import { useKeepAwake } from 'expo-keep-awake';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
-
+  useKeepAwake();
   return (
-    <Layout />
+    <><StatusBar hidden /><Layout /></>
   );
 }
